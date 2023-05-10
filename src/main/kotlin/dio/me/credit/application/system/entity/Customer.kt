6bf0e4d32dbf8a12b0054335f35ad9258b7e62f1
f4k3z0rd <vinicius.meireles@outlook.com>
2023-05-10 13:04:23 -0300

    Cria endpoint do Customer com: - save - findById - deleteCustomer - updateCustomer

--- src/main/kotlin/dio/me/credit/application/system/entity/Customer.kt
+++ src/main/kotlin/dio/me/credit/application/system/entity/Customer.kt
@@ -1,6 +1,7 @@
 package dio.me.credit.application.system.entity
 
 import jakarta.persistence.*
+import java.math.BigDecimal
 
 @Entity
 @Table(name="Customer")
@@ -11,6 +12,7 @@ data class Customer(
     @Column(nullable = false, unique = true) var email: String = "",
     @Column(nullable = false) var password: String = "",
     @Column(nullable = false) @Embedded var address: Address = Address(),
+    @Column(nullable = false) var income: BigDecimal = BigDecimal.ZERO,
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE, CascadeType.PERSIST], mappedBy = "customer") var credits: List<Credit> = mutableListOf(),
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
