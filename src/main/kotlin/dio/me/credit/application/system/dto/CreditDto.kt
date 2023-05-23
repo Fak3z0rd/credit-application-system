de46e8cee1218bb6fe7544c025f78386b5831897
f4k3z0rd <vinicius.meireles@outlook.com>
2023-05-23 12:40:20 -0300

    Adiciona testes de integração ao banco de dados do CreditResource

--- src/main/kotlin/dio/me/credit/application/system/dto/CreditDto.kt
+++ src/main/kotlin/dio/me/credit/application/system/dto/CreditDto.kt
@@ -5,7 +5,6 @@ import dio.me.credit.application.system.entity.Customer
 import jakarta.validation.constraints.Future
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import java.math.BigDecimal
 import java.time.LocalDate
